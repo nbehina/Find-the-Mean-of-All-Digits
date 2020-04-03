0f65ca4a0217e056a5838eb0af57741093dd6806
Your Namenbehina <nbehina@gmail.com>
2020-04-02 23:05:45 -0600

    Find the Mean of All Digits

---
+++ main.js
@@ -0,0 +1,8 @@
+function division(_a) {
+    var numOne = _a[0], numTwo = _a[1];
+    var mean = (numOne + numTwo) / 2;
+    console.log(mean);
+}
+;
+division([4, 2]);
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,7 @@
+
+function division([numOne, numTwo]: [number, number]) { 
+  let mean: number = (numOne + numTwo)/2; 
+  console.log(mean);
+};
+division([4, 2]);
+
